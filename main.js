5673a128d4fbacf84324d1c6aac2ae31e10fe384
anthony-mendola <anthonymendola@gmail.com>
2018-12-09 21:04:40 -0500

    created block function with notes

---
+++ main.js
@@ -0,0 +1,9 @@
+//index is optional - tells where the block sits on the chain
+//timestamp = when the block was created
+//data = any type of data associated with this block, money transferred sender receiver
+//previosHash is a string that contains the hash of the block before this one, ensures the integrity of the block chain.
+class Block{
+  constructor(index, timestamp, data, previousHash = '') {
+
+  }
+}
