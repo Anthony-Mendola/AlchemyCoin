9ed8ff48807cb712c9e679eab662542f7c461cae
anthony-mendola <anthonymendola@gmail.com>
2018-12-09 21:20:17 -0500

    calculate hash function comments

--- main.js
+++ main.js
@@ -4,6 +4,16 @@
 //previosHash is a string that contains the hash of the block before this one, ensures the integrity of the block chain.
 class Block{
   constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = '';
+  }
+//calculates the hash function of the block
+//takes the properties of the block runs through the hash function and returns the hash which identifies our block on the blockchain
+  calculateHash() {
 
   }
+
 }
